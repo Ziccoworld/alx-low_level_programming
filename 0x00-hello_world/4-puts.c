d39d79e11f69331ecf0ed64d95134aaad085fef4
Ziccoworld <Mosesisaacabi@gmail.com>
2023-03-16 17:28:38 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main  - Main function
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
+
 int main(void)
 {
-        puts("\"programming is like a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
