ef7696f9e90843e5561f746923eae8187eac5ae5
Ziccoworld <Mosesisaacabi@gmail.com>
2023-03-20 04:01:30 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,33 +3,29 @@
 #include <stdio.h>
 
 /**
- * main - prints the last digit of a randomly generated numbrt
- * and whether it is greater than 5, less than 6, or 0.
- *
- * Return: Alwalys 0.
- */
+  *main - Entry point
+  *Description: 'get the last degiti of a number'
+  *Return: alwalys 0
+  */
 int main(void)
-{ 
+{
 	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if ((n % 10) > 5)
+	ld = n % 10;
+	if (ld > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n",
-				n, n% 10);
+		printf("last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
+	else if (ld == 0)
 	{
-		printf("last digit of %d is %d and is less than 6 and not0\n",
-				n, n % 10);
+		printf("last digit of %d is %d and is 0\n", n, ld);
 	}
 	else
 	{
-		printf("last digit of %d is %d and 0\n",
-				n, n % 10);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
-
 	return (0);
 }
