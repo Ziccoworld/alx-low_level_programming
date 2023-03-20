ecb30b5d83e8c21b6061a14d575c23ac98328be8
Ziccoworld <Mosesisaacabi@gmail.com>
2023-03-20 05:51:02 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-        int n, m, l;
+int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
@@ -28,6 +28,6 @@ int main(void)
 			}
 		}
 	}
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
