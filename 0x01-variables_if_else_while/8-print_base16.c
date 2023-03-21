e8965c64f5e4d61c0ad24f9ab2dcc0a739baf9aa
Ziccoworld <Mosesisaacabi@gmail.com>
2023-03-20 17:46:28 -0700

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,18 +7,17 @@
  */
 int main(void)
 {
-int n;
-int m;
+	int n;
+	int m;
 
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
 	}
 	for (m = 97; m <= 102; m++)
-		{
-			putchar(m);
-		}
+	{
+		putchar(m);
 	}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
