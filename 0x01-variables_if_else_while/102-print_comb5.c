220224cbd763920711e0c48e335d4f8476c969c2
Ziccoworld <Mosesisaacabi@gmail.com>
2023-03-20 18:02:54 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
-
 /**
  * main - prints all possible combinations of two two-digit numbers
- * Return: Alwalys 0.
+ *
+ * Return: 0
  */
 int main(void)
 {
-        int i, j;
-
-	for (i = 0; i < 100; i++)
-	{
-		for (j = 0; j < 100; j++)
-		{
-			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((i % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+        int num1, num2;
+        for (num1 = 0; num1 <= 99; num1++)
+        {
+                for (num2 = num1; num2 <= 99; num2++)
+                {
+                        if (num1 != num2)
+                        {
+                                putchar(num1 / 10 + '0');
+                                putchar(num1 % 10 + '0');
+                                putchar(' ');
+                                putchar(num2 / 10 + '0');
+                                putchar(num2 % 10 + '0');
+                                if (num1 < 98 || num2 < 99)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
