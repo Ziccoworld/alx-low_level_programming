6ecfa13aac3f9a0a8cc26b6db5fcdf6195de305f
Ziccoworld <Mosesisaacabi@gmail.com>
2023-03-22 02:53:40 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
-#inlude "main.h"
+#include "main.h"
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-return (0);
+	return (0);
 }
