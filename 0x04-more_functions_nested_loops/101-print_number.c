681be02c99e54c097d0d6adec2fe5f8f9bfb4f3f
Ziccoworld <Mosesisaacabi@gmail.com>
2023-03-28 21:16:56 -0700

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,28 +7,21 @@
 
 void print_number(int n)
 {
-	int copy, nth, size = 1, ones = n % 10;
+	unsigned int n1;
 
-	n /= 10;
-	copy = n;
-	if (ones < 0)
+	if (n < 0)
 	{
-		ones *= -1, copy *= -1, n *= -1;
+		n1 = -n;
 		_putchar('-');
 	}
-	if (copy > 0)
+	else
 	{
-		while (copy / 10 != 0)
-		{
-			copy /= 10, size *= 10;
-		}
-		while (size > 0)
-		{
-			nth = n / size;
-			_putchar('0' + nth);
-			n -= nth * size;
-			size /= 10;
-		}
+		n1 = n;
 	}
-	_putchar('0' + ones);
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
